7d270184e84e879346d597e34322145c3a5bb70b
Yuval <yuyefet@g.jct.ac.il>
2023-03-27 17:29:59 +0300

    Starting tests

---
+++ unittests/primitives/PointTests.java
@@ -0,0 +1,28 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTests {
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void testTestEquals() {
+    }
+
+    @Test
+    void testTestToString() {
+    }
+
+    @Test
+    void testDistance() {
+    }
+
+    @Test
+    void testSubtract() {
+    }
+}
---
+++ unittests/primitives/VectorTests.java
@@ -0,0 +1,44 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTests {
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void testScale() {
+    }
+
+    @Test
+    void testCrossProduct() {
+    }
+
+    @Test
+    void testDotProduct() {
+    }
+
+    @Test
+    void testLengthSquared() {
+    }
+
+    @Test
+    void testLength() {
+    }
+
+    @Test
+    void testNormalize() {
+    }
+
+    @Test
+    void testTestToString() {
+    }
+
+    @Test
+    void testTestEquals() {
+    }
+}

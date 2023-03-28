a72fada2445b4d44ecd5115d80aa50ac38389063
Yuval <yuyefet@g.jct.ac.il>
2023-03-28 18:04:44 +0300

    Commentery

--- unittests/geometries/PlaneTests.java
+++ unittests/geometries/PlaneTests.java
@@ -14,17 +14,20 @@ class PlaneTests {
     public void testConstructor() {
         // ============ Equivalence Partitions Test ==============
         // TC01: All the points are the same
+        // try to crate a plane
 
         try {
             new Plane(new Point(0, 0, 1), new Point(2, 0, 0), new Point(0, 4, 0));
         } catch (IllegalArgumentException e) {
             fail("Failed constructing a correct Plane");
         }
-        //BVA
+        //============ BVA ============
+        //TC11: two points are the same
         assertThrows(IllegalArgumentException.class,
                 () -> new Plane(new Point(1,2,3), new Point(1,2,3), new Point(1,2,6)), //
                         "Error in ctr when two points are the same");
 
+        //TC12: all the points are on the same line
         assertThrows(IllegalArgumentException.class,
                 () -> new Plane(new Point(1,2,3), new Point(2,4,6), new Point(-1,-2,-3)), //
                 "Error in ctr when all the points are on the same line");
@@ -36,7 +39,6 @@ class PlaneTests {
     void getNormal() {
         // ============ Equivalence Partitions Tests ==============
         // TC01: All the points are the same
-        // Create a plane with equal points
         final Plane plane = new Plane(new Point(0, 0, 1), new Point(2, 0, 0), new Point(0, 4, 0));
         assertEquals(new Vector(4, 2, 8).normalize(), plane.getNormal(), "The normal of the plane is not normalized");
     }

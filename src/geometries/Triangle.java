cd0e3104d1dd2332b6178215e8d6194f981cbe4b
jeremiet3 <jeremiet3@gmail.com>
2023-04-18 21:22:25 +0300

    Triangle Test almost !

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -2,6 +2,7 @@ package geometries;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Vector;
 
 import java.util.List;
 
@@ -18,6 +19,44 @@ public class Triangle extends Polygon {
 
     @Override
     public List<Point> findIntersections(Ray ray){
+
+        List<Point> list = this.plane.findIntersections(ray);
+        Vector v1,v2,v3;
+        Vector n1,n2,n3;
+        Vector v=ray.getDir();
+        Point p1= this.vertices.get(0);
+        Point p2= this.vertices.get(1);
+        Point p3= this.vertices.get(2);
+        try {
+            v1=p1.subtract(ray.getP0());
+            v2=p2.subtract(ray.getP0());
+            v3=p3.subtract(ray.getP0());
+        }
+        catch(IllegalArgumentException ex)
+        {
+            return null;
+        }
+
+        try{
+            n1=v1.crossProduct(v2).normalize();
+            n2=v2.crossProduct(v3).normalize();
+            n3=v3.crossProduct(v1).normalize();
+        }
+        catch(IllegalArgumentException ex)
+        {
+            return null;
+        }
+
+        double vn1,vn2,vn3;
+
+        vn1= v.dotProduct(n1);
+        vn2= v.dotProduct(n2);
+        vn3= v.dotProduct(n3);
+
+        if((vn1<0 &&vn2<0 && vn3<0)|| (vn1>0 && vn2>0 && vn3>0))
+        {
+            return list;
+        }
         return null;
     }
 }
--- unittests/geometries/TriangleTests.java
+++ unittests/geometries/TriangleTests.java
@@ -1,7 +1,10 @@
 package geometries;
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,4 +22,35 @@ class TriangleTests {
         assertEquals(new Vector(4, 2, 8).normalize(), tr.getNormal(new Point(0,0,0)), "The normal of the triangle is not normalized");
     }
 
+    @Test
+    void testFindIntersections() {
+        Point[] pts =
+                {new Point(0, 0, 1), new Point(2, 0, 0), new Point(0, 2, 0)};
+        final Triangle triangle = new Triangle(pts[0],pts[1],pts[2]);
+        Point intersect;
+
+        // ============ Equivalence Partitions Tests ==============
+        // TC01 : Inside polygon/triangle
+        intersect = new Point(0.39,1.16,0);
+        assertEquals(List.of(intersect),triangle.findIntersections(new Ray(new Point(0,0,1),new Vector(0.39,1.16,-1))),"there is one intersection");
+
+        // TC02 : outside against edge
+        assertNull(triangle.findIntersections(new Ray(new Point(0,0,1),new Vector(1,-0.5,-1))),"there is no intersection");
+
+        // TC03 : outside against vertex
+        assertNull(triangle.findIntersections(new Ray(new Point(0,0,1),new Vector(2.49,-0.23,-1))),"there is no intersection");
+
+        //============ BVA ============
+        // The ray begins "before" the plane
+        // TC11 : On edge
+        assertNull(triangle.findIntersections(new Ray(new Point(0,0,1),new Vector(1,0,-1))),"there is no intersection");
+
+        // TC12 : In Vertex
+        assertNull(triangle.findIntersections(new Ray(new Point(0,0,1),new Vector(2,0,-1))),"there is no intersection");
+
+        // TC13 : On edge's continuation
+        assertNull(triangle.findIntersections(new Ray(new Point(0,0,1),new Vector(0,-0.5,-1))),"there is no intersection");
+
+
+    }
 }

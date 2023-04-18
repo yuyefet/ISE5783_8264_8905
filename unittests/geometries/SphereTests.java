d57ffc580f4ce7cd24b1fd2f0390e3ab0e292b06
yuyefet <yuyefet@g.jct.ac.il>
2023-04-18 11:09:34 +0300

    Sphere tests

--- unittests/geometries/SphereTests.java
+++ unittests/geometries/SphereTests.java
@@ -2,9 +2,14 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
+import java.util.ArrayList;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SphereTests {
     /** Test method for {@link geometries.Sphere#Sphere(double radius,Point center)}. */
@@ -26,5 +31,30 @@ class SphereTests {
         // TC01: All the points are the same
         final Sphere sphere = new Sphere(2,new Point(1,1,1));
         assertEquals(new Vector(2, 0,0 ).normalize(), sphere.getNormal(new Point(3,1,1)), "The normal of the sphere is not normalized");
+    }
+
+    /**
+     * Test method for {@link geometries.Sphere#findIntersections(Ray)}.
+     */
+    @Test
+    void testFindIntersectionPoints() {
+        final Sphere sphere = new Sphere(2,new Point(0,0,0));
+
+        // ============ Equivalence Partitions Test ==============
+        // TC01 : A ray on the line that is completely off the sphere
+
+        Ray ray = new Ray(new Point(-3,0,0), new Vector(1,3,0));
+        assertNull(sphere.findIntersections(ray), "There is no intersection!");
+
+        // TC02 : A ray on the straight line that intersect with the sphere,
+        // when the beginning of the ray is before the sphere
+        ray = new Ray(new Point(3,-3,0), new Vector(-7,4,0));
+        List<Point> intersections = new ArrayList<>();
+        intersections.add(new Point(0.89, -1.79, 0));
+        intersections.add(new Point(-1.99, -0.15, 0));
+        assertEquals(, sphere.findIntersections(ray), "" );
+
+
+
     }
 }

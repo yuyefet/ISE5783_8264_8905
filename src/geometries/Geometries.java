303b771eeff6a8ff0ff1337a45473d0c5d8cc075
jeremiet3 <jeremiet3@gmail.com>
2023-04-29 22:59:24 +0300

    PR03

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -21,9 +21,8 @@ public class Geometries implements Intersectable{
 
     public void add(Intersectable ... geometries){
         for(Intersectable geo : geometries)
-        {
             intersections.add(geo);
-        }
+
     }
 
     @Override

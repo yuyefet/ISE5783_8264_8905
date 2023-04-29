74f7594ed678e87b6e41f28af2f99ccdb2caa6ab
yuyefet <yuyefet@g.jct.ac.il>
2023-04-29 22:34:27 +0300

    Geometries tests

--- unittests/geometries/GeometriesTests.java
+++ unittests/geometries/GeometriesTests.java
@@ -2,6 +2,9 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,13 +12,22 @@ class GeometriesTests {
 
     @Test
     void findIntersections() {
-        // ********** BVA *************
-        // Empty collection of geometries
-        Geometries geometries= new Geometries(new Sphere(2,new Point(1,1,1)),new Plane());
+        Geometries geometries= new Geometries();
+        Plane plane = new Plane(new Point(-5, 0, 0), new Point(0, -4,0), new Point(0,0,4));
+        Triangle triangle = new Triangle(new Point(0,0,3), new Point(-3,0,0), new Point(0,3,0));
+        Sphere sphere = new Sphere(2, new Point(2,0,0));
+        List<Point> result;
 
-
+        Ray ray = new Ray(new Point(0,0,1), new Vector(0,1,0));
         // ********** BVA *************
-        // No intersections
+        // TC11: Empty collection of geometries
+        assertNull(geometries.findIntersections(ray), "There are no intersections");
+
+        // TC12: No intersections
+        geometries= new Geometries(plane, triangle, sphere);
+        result = geometries.findIntersections(ray);
+
+        assertEquals(0, result.size());
 
 
 

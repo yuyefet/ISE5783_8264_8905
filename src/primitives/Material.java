432af44fc2a76bb644e63e5bfae744025787f25a
yuyefet <yuyefet@g.jct.ac.il>
2023-05-29 20:07:48 +0300

    stage 7 - beginning part 2

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -1,8 +1,13 @@
 package primitives;
 
 public class Material {
-    public Double3 kD = Double3.ZERO, kS = Double3.ZERO;
-    public int nShininess = 0;
+    public Double3 kD = Double3.ZERO; //diffuse reflection coefficient
+    public Double3 kS = Double3.ZERO; //specular reflection
+    public Double3 kT = Double3.ZERO; //transparency coefficient
+    public Double3 kR = Double3.ZERO; //reflection coefficient
+    public int nShininess = 0; //shininess coefficient of the material
+
+
 
 //----- Setters - returning this -----------//
 
@@ -49,6 +54,21 @@ public class Material {
 
     public Material setnShininess(int nShininess) {
         this.nShininess = nShininess;
+        return this;
+    }
+
+    /**
+     *
+     * @param kT
+     * @return
+     */
+    public Material setkT(Double3 kT) {
+        this.kT = kT;
+        return this;
+    }
+
+    public Material setkR(Double3 kR) {
+        this.kR = kR;
         return this;
     }
 }
